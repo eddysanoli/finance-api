b670245fa4c6eb9c6c18eec68095ece8ab1e7214
eddysanoli <eddysanoli@gmail.com>
2025-09-11 22:05:53 -0600

    Database Setup
    I think I managed to fit in all of the needs of the project into a
    database, so I included a schema and some initial data, and will try to
    test the database with a docker container

---
+++ db/schema.sql
@@ -0,0 +1,177 @@
+-- =================================================
+--   PRODUCT AND BRAND                             
+-- =================================================
+
+CREATE TABLE brand (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE product (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    brand_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+
+    FOREIGN KEY (brand_id) REFERENCES brand(id) ON DELETE SET NULL
+);
+
+CREATE TABLE product_price (
+    id SERIAL PRIMARY KEY,
+    product_id INT,
+    price DECIMAL(10, 2) NOT NULL,
+    valid_from DATE NOT NULL DEFAULT CURRENT_DATE,
+    valid_to DATE,
+
+    FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE CASCADE
+)
+
+-- =================================================
+--   LOCATION                                      
+-- =================================================
+
+CREATE TABLE location (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    address TEXT,
+    coordinates geography(POINT, 4326),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- =================================================
+--   BUSINESS                                      
+-- =================================================
+
+CREATE TABLE business (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- =================================================
+--   BENEFICIARY                                   
+-- =================================================
+
+-- Relationship with an entity (cousin, sibling, etc.)
+CREATE TABLE relationship (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- Entities can be both individuals or objects. For example, a car or a house can be considered an entity.
+CREATE TABLE entity (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)
+
+-- Multiple relationships can be associated with an entity, for example, a family member
+-- can be both part of "family" and "cousin" relationships.
+CREATE TABLE relationship_entity (
+    relationship_id INT,
+    entity_id INT,
+    CONSTRAINT id PRIMARY KEY (relationship_id, entity_id),
+
+    FOREIGN KEY (relationship_id) REFERENCES relationship(id) ON DELETE CASCADE,
+    FOREIGN KEY (entity_id) REFERENCES entity(id) ON DELETE CASCADE
+);
+
+-- =================================================
+--   OCCASION                                      
+-- =================================================
+
+CREATE TABLE occasion (
+    id SERIAL PRIMARY KEY,
+    short_name VARCHAR(100) NOT NULL UNIQUE,
+    description TEXT,
+    date DATE NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- =================================================
+--   TRANSACTION                                   
+-- =================================================
+
+CREATE TABLE category (
+    id SERIAL PRIMARY KEY,
+    slug VARCHAR(100) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- Periodicity for recurring transactions (in the form of CRON expressions)
+CREATE TABLE periodicity (
+    id SERIAL PRIMARY KEY,
+    minute INT CHECK (minute BETWEEN 0 AND 59),
+    hour INT CHECK (hour BETWEEN 0 AND 23),
+    day_of_month INT CHECK (day_of_month BETWEEN 1 AND 31),
+    month INT CHECK (month BETWEEN 1 AND 12),
+    day_of_week INT CHECK (day_of_week BETWEEN 0 AND 6),
+)
+
+CREATE TABLE transaction (
+    id SERIAL PRIMARY KEY,
+    description TEXT,
+    context TEXT,
+    quantity INT NOT NULL DEFAULT 1,
+    amount DECIMAL(10, 2) NOT NULL,
+    type VARCHAR(50) NOT NULL CHECK (type IN ('income', 'expense', 'transfer')),
+
+    invoice_id VARCHAR(100) UNIQUE,
+    location_id INT,
+    periodicity_id INT,
+
+    beneficiary_type VARCHAR(50) NOT NULL CHECK (beneficiary_type IN ('business', 'person')),
+    beneficiary_id INT,
+    payer_type VARCHAR(50) NOT NULL CHECK (payer_type IN ('business', 'person')),
+    payer_id INT,
+
+    transaction_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_DATE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+
+    FOREIGN KEY (location_id) REFERENCES location(id) ON DELETE SET NULL
+);
+
+-- Many-to-Many relationship between transactions and occasions
+CREATE TABLE transaction_occasion (
+    transaction_id INT,
+    occasion_id INT,
+    CONSTRAINT id PRIMARY KEY (transaction_id, occasion_id),
+
+    FOREIGN KEY (transaction_id) REFERENCES transaction(id) ON DELETE CASCADE,
+    FOREIGN KEY (occasion_id) REFERENCES occasion(id) ON DELETE CASCADE
+);
+
+-- Many-to-Many relationship between transactions and categories
+CREATE TABLE transaction_category (
+    transaction_id INT,
+    category_id INT,
+    CONSTRAINT id PRIMARY KEY (transaction_id, category_id),
+
+    FOREIGN KEY (transaction_id) REFERENCES transaction(id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE CASCADE
+);
+
+-- Many-to-Many relationship between transactions and products
+-- (a transaction can have multiple products and a product can be part of multiple transactions)
+CREATE TABLE transaction_product (
+    transaction_id INT,
+    product_id INT,
+    CONSTRAINT id PRIMARY KEY (transaction_id, product_id),
+
+    FOREIGN KEY (transaction_id) REFERENCES transaction(id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE CASCADE
+)
+
