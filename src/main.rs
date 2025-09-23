25e17b358b4b0931c4f197f335ce948974eb6c24
eddysanoli <eddysanoli@gmail.com>
2025-09-22 22:01:21 -0600

    Logging and Healthcheck Endpoint
    Added logging to my API endpoint, along with a test endpoint that works
    as a healthcheck

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-use axum::{routing::get, Router);
+use axum::{Json, Router, response::IntoResponse, routing::get};
+use log;
+use serde_json;
+use env_logger;
 
 #[tokio::main]
 async fn main() {
+    env_logger::init();
+    log::info!("Configured logger");
+
+    // Setup the router and start the server
     let addr = "127.0.0.1:3000";
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     println!("Listening on http://{}", addr);
@@ -9,9 +16,14 @@ async fn main() {
 }
 
 fn router() -> Router {
-    Router::new().route("/", get(hello_world))
+    Router::new().route("/api/v1/healthcheck", get(health_check_handler))
 }
 
-async fn hello_world() -> &'static str {
-    "Hello, World!"
+async fn health_check_handler() -> impl IntoResponse {
+    log::info!(target: "healthcheck", "Health check endpoint called");
+    let json_response = serde_json::json!({
+        "status": "ok",
+        "message": "Service is running"
+    });
+    return Json(json_response);
 }
